bf88847fce74281ac52f44b0c211329e4d2e746c
franklin <franklin@weareinteractive.com>
2015-09-04 15:43:24 +0200

    updated travis

--- test/run.sh
+++ test/run.sh
@@ -3,6 +3,8 @@
 PWD=$(pwd)
 ROLE_NAME=franklinkim.apache2
 
+echo 'creating role symlink'
 ln -s $(dirname $PWD) /etc/ansible/roles/$ROLE_NAME
 
+echo 'running playbook'
 ansible-playbook -vvvv -i 'localhost,' -c local $PWD/test/playbook.yml
