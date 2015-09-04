6d49ff1b7a4b3d5d364df177f6c043ee37bdd11d
franklin <franklin@weareinteractive.com>
2015-09-04 16:01:51 +0200

    updated travis

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
+echo '............'
+echo $CI_HOME
+echo $ROLE_NAME
+
+
 PWD=$(pwd)
 ROLE_NAME=franklinkim.apache2
+
+ls -la /etc
+
+ls -la /etc/ansible
 
 echo 'creating role symlink'
 ln -s $(dirname $PWD) /etc/ansible/roles/$ROLE_NAME
