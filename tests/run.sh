5f8fc61b58e687308973bd5f72b28d35bf1d98a6
franklin <franklin@weareinteractive.com>
2015-09-05 18:51:49 +0200

    updated tests

--- tests/run.sh
+++ tests/run.sh
@@ -3,16 +3,11 @@
 PWD=$(pwd)
 ROLE_NAME=franklinkim.apache2
 
-ls -la /usr/share
-
-ls -la /usr/share/ansible
-
 echo 'creating role symlink'
 ln -s $(dirname $PWD) /usr/share/ansible/roles/$ROLE_NAME
 
-ls -la /usr/share/ansible/roles
-
-cat /etc/ansible/ansible.cfg
+ls -la /usr/share/ansible/roles/$ROLE_NAME/
+ls -la /usr/share/ansible/roles/$ROLE_NAME/tasks
 
 echo 'running playbook'
 ansible-playbook -vvvv -i 'localhost,' -c local $PWD/tests/playbook.yml
