1fb6fe3f927f8c026f44b0f6ccf4b5033e97cd66
franklin <franklin@weareinteractive.com>
2015-09-06 10:35:10 +0200

    added dependencies

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 PWD=$(pwd)
-ROLE_NAME2=franklinkim.apache2
+ROLE_NAME=franklinkim.apache2
 
-echo "--------------"
-echo $ROLE_NAME
-
-echo ansible --version
+echo 'installing dependencies'
+ansible-galaxy install franklinkim.openssl
+ansible-galaxy install franklinkim.htpasswd
 
 echo 'creating role symlink'
-ln -s $PWD /usr/share/ansible/roles/$ROLE_NAME2
+ln -s $PWD /usr/share/ansible/roles/$ROLE_NAME
 
 echo 'running playbook'
 ansible-playbook -vvvv -i 'localhost,' -c local $PWD/tests/playbook.yml
