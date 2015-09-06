35a0f13af823e345591dba6ece9fe047f773f41f
franklin <franklin@weareinteractive.com>
2015-09-06 10:23:41 +0200

    added env

--- tests/run.sh
+++ tests/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 PWD=$(pwd)
-ROLE_NAME=franklinkim.apache2
+ROLE_NAME2=franklinkim.apache2
+
+echo "--------------"
+echo $ROLE_NAME
+
+echo ansible --version
 
 echo 'creating role symlink'
-ln -s $PWD /usr/share/ansible/roles/$ROLE_NAME
-
-echo 'debug'
-echo $PWD
-ls -la /usr/share/ansible/roles/
-ls -la /usr/share/ansible/roles/$ROLE_NAME/
-ls -la /usr/share/ansible/roles/$ROLE_NAME/tasks
+ln -s $PWD /usr/share/ansible/roles/$ROLE_NAME2
 
 echo 'running playbook'
 ansible-playbook -vvvv -i 'localhost,' -c local $PWD/tests/playbook.yml
